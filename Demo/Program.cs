f1427d229c28ae7ab5e9bafc4cb56772ac2dce4a
Corey Sutton <coreysutton@outlook.co.nz>
2019-04-23 19:23:11 +1200

    Create base solution

---
+++ Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CoreySutton.PrettyCon;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        private static PrettyCon pCon;
+        static void Main()
+        {
+            pCon = new PrettyCon();
+            Print();
+
+            pCon.EnableStopwatch = true;
+            Print();
+
+            pCon.EnableTimestamp = true;
+            Print();
+
+            pCon.EnableStopwatch = false;
+            Print();
+
+            pCon.EnableTimestamp = false;
+            Print();
+
+            Console.WriteLine("Press <Enter> to close");
+            Console.ReadLine();
+        }
+
+        private static void Print()
+        {
+            pCon.Debug("This is a debug message");
+            pCon.Verbose("This is a verbose message");
+            pCon.Info("This is a information message");
+            pCon.Info("This is a coloured information message", ConsoleColor.Magenta);
+            pCon.Warning("This is a warning message");
+            pCon.Error("This is an error message");
+            pCon.Exception(new Exception("This is an exception message"));
+            pCon.Status("This is a status", "SUCCESS", ConsoleColor.Green);
+            pCon.Status("This is a status", "SKIPPED", ConsoleColor.Yellow);
+            pCon.Status("This is a status", "FAILURE", ConsoleColor.Red);
+            pCon.Status(
+                "This is a very very very very very very long status that will " +
+                "take up a lot of space in the console output", 
+                "SKIPPED", 
+                ConsoleColor.Yellow);
+            pCon.Status("This is a very very very very very very very very very " +
+                "very very very very very very very very very very very very " +
+                "very very very very very very very very very very very very " +
+                "very very very long status that will take up a lot of space " +
+                "in the console output", 
+                "FAILURE", 
+                ConsoleColor.Red);
+            pCon.Break();
+        }
+    }
+}
---
+++ PrettyCon/PrettyLogger.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CoreySutton.PrettyCon
+{
+    public class PrettyCon
+    {
+        private Stopwatch _stopwatch;
+        private bool _enableStopwatch = false;
+        private const ConsoleColor _defaultColor = ConsoleColor.Gray;
+
+        public bool EnableTimestamp;
+
+        public bool EnableStopwatch
+        {
+            get => _enableStopwatch;
+            set
+            {
+                _enableStopwatch = value;
+                if (value)
+                {
+                    _stopwatch = new Stopwatch();
+                    _stopwatch.Start();
+                }
+                else
+                {
+                    _stopwatch.Stop();
+                }
+            }
+        }
+
+        public void Debug(string message, ConsoleColor color = _defaultColor)
+        {
+            Print(message, color, "DEBUG", ConsoleColor.DarkBlue);
+        }
+
+        public void Verbose(string message, ConsoleColor color = _defaultColor)
+        {
+            Print(message, color, "VERBOSE", ConsoleColor.Blue);
+        }
+
+        public void Info(string message, ConsoleColor color = _defaultColor)
+        {
+            Print(message, color, "INFO", ConsoleColor.Cyan);
+        }
+
+        public void Warning(string message, ConsoleColor color = _defaultColor)
+        {
+            Print(message, color, "WARNING", ConsoleColor.Yellow);
+        }
+
+        public void Error(string message, ConsoleColor color = _defaultColor)
+        {
+            Print(message, color, "ERROR", ConsoleColor.Red);
+        }
+
+        public void Exception(Exception ex)
+        {
+            if (ex == null) return;
+
+            Print(ex.Message, _defaultColor, "EXCEPTION", ConsoleColor.DarkRed);
+
+            Exception innerEx = ex.InnerException;
+            while (innerEx != null)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(innerEx.Message);
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine(innerEx.StackTrace);
+            }
+
+            Console.ForegroundColor = _defaultColor;
+        }
+
+        public void Break(int length = 80)
+        {
+            string output = "";
+            for (int i = 0; i < length; i++)
+            {
+                output += "-";
+            }
+
+            Console.WriteLine(output);
+        }
+
+        public void Status(string message, string status, ConsoleColor color)
+        {
+            if (string.IsNullOrEmpty(message) || string.IsNullOrEmpty(status)) return;
+
+            string timestampOutput = PrintTimestamp();
+            Console.Write(message);
+            string stopwatchOutput = PrintStopwatch();
+            
+            // Calculate status width
+            int statusPaddingLength = 2;
+            int newLineCharacterLength = 1;
+            int statusWidth = status.Length + statusPaddingLength + newLineCharacterLength;
+
+            // Calculate total message length
+            int totalMessageLength = message.Length;
+            if (!string.IsNullOrEmpty(timestampOutput)) totalMessageLength += timestampOutput.Length;
+            if (!string.IsNullOrEmpty(stopwatchOutput)) totalMessageLength += stopwatchOutput.Length;
+
+            // Calculate the status offset
+            int offset;
+            if (totalMessageLength + statusWidth <= Console.WindowWidth || 
+                totalMessageLength > Console.WindowWidth)
+            {
+                offset = Console.WindowWidth - statusWidth;
+            }
+            else
+            {
+                // Force the annoation to be at the end of the next line
+                Console.WriteLine();
+                offset = Console.WindowWidth - statusWidth;
+            }
+
+            Console.ForegroundColor = color;
+            Console.CursorLeft = offset;
+            Console.WriteLine($"[{status}]");
+            Console.ForegroundColor = _defaultColor;
+        }
+
+        private void Print(string message, ConsoleColor messageColor, string type, ConsoleColor typeColor)
+        {
+            if (message == null || string.IsNullOrEmpty(message)) return;
+
+            PrintTimestamp();
+
+            Console.ForegroundColor = typeColor;
+            Console.Write($"[{type}] ");
+
+            Console.ForegroundColor = messageColor;
+            Console.Write(message);
+
+            PrintStopwatch();
+
+            Console.WriteLine();
+        }
+
+        private string PrintTimestamp()
+        {
+            string timestampOutput = "";
+            if (EnableTimestamp)
+            {                
+                timestampOutput = $"[{DateTime.Now.ToString("HH:mm:ss")}] ";
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(timestampOutput);
+                Console.ForegroundColor = _defaultColor;
+            }
+
+            return timestampOutput;
+        }
+
+        private string PrintStopwatch()
+        {
+            string stopwatchOutput = "";
+            if (EnableStopwatch)
+            {
+                stopwatchOutput = $" [{_stopwatch.ElapsedMilliseconds} ms]";
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(stopwatchOutput);
+                Console.ForegroundColor = _defaultColor;
+            }
+
+            return stopwatchOutput;
+        }
+    }
+}
